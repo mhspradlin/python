f3ef3415a9192aad2c3efd7a181d7bb136a06385
mhs <mitch@spradlin.net>
2013-12-30 18:47:14 -0600

    Implemeted handling of newlines for rendering the body text and the choice text. Still needs a little ironing out.

--- TheAdventuresofBobtheTurtle(TurtleBob)/main.py
+++ TheAdventuresofBobtheTurtle(TurtleBob)/main.py
@@ -10,6 +10,9 @@ import time
 import atexit
 #Import terminalsize.py so we can get the size of the console
 import terminalsize
+#Import the random module so we can do things with randomness, like present
+# the choices in a random order
+import random
 
 def main():
     print("Let the adventure begin!")
@@ -24,8 +27,9 @@ def renderscreen():
     #Set some of our variables
     #These will end up being passed as arguments to the renderscreen function
     title = "Introduction"
-    bodytext = "As we begin our adventure, the narrator, in a calm and soothing voice, explains some details about our adventurer."
-    
+    bodytext = "As we begin our adventure, the narrator, in a calm and soothing voice, explains some details about our adventurer.\n\nWhat is your name?"
+    choices = ["Marceline", "John\nPierre III", "Tuccidides", "Paptcho"]
+
     #Get the screen attributes
     (width, height) = terminalsize.get_terminal_size()
     #Calculate the indent for the bodytext
@@ -36,6 +40,10 @@ def renderscreen():
     bodywidth = int(0.8 * width)
     #Calculate the indent for the "Press a Key" message
     contindent = int((width - len("Press a Key"))/2)
+    #Calculate the indent for the choices
+    choiceindent = int(0.05 * width)
+    #Calculate with width of the choice lines
+    choicewidth = int(0.9 * width)
 
     #This will print '=' across the width of the screen to make a top border
     for i in range(width-1):
@@ -48,22 +56,35 @@ def renderscreen():
     print(title)
     print("")
 
-    #This will print out the bodytext line by line, stopping if it would run past
-    # the end of the terminal
+    #This will print out the bodytext line by line, gracefully handling newlines,
+    # stopping if it would run past the end of the terminal
     place = 0
     k = 3
+    isnew = False
     while place < len(bodytext):
         for i in range(bodyindent):
             print(" ", end="")
         for i in range(bodywidth):
+            #This first clause checks to see if the next character is a newline and
+            # handles it appropriately
+            if place + i <= len(bodytext) - 1 and bodytext[place + i] == "\n":
+                for l in range(i, bodywidth - 1):
+                    print(" ", end="")
+                print(" ")
+                #Skip rendering the newline character
+                place += i + 1
+                isnew = True
+                break #Leaves the smallest enclosing for/while statement
             if place + i > len(bodytext) - 1:
                 print(" ", end="")
-                break #Leaves the smallest enclosing for/while statement
+                break
             print(bodytext[place + i], end="")
-        place += int(0.8 * width)
-        for i in range(bodyindent - 1):
-            print(" ", end="")
-        print(" ")
+        if isnew == False:
+            place += int(0.8 * width)
+            for i in range(bodyindent - 1):
+                print(" ", end="")
+            print(" ")
+        isnew = False
         if k >= height - 2:
             print("")
             for i in range(contindent):
@@ -72,6 +93,46 @@ def renderscreen():
         k += 1
     print("")
 
+    #Render the choices, with one line of space in between them, indicating
+    # the selected option
+    #Create a list of the choices, randomized
+    randchoices = random.sample(choices, len(choices))
+    for currentchoice in randchoices:
+        place = 0
+        while place < len(currentchoice):
+            for i in range(choiceindent):
+                print(" ", end="")
+            for i in range(choicewidth):
+                #This first clause checks to see if the next character is a newline and
+                # handles it appropriately
+                if place + i <= len(currentchoice) - 1 and currentchoice[place + i] == "\n":
+                    for l in range(i, choicewidth - 1):
+                        print(" ", end="")
+                    print(" ")
+                    #Skip rendering the newline character
+                    place += i + 1
+                    isnew = True
+                    break #Leaves the smallest enclosing for/while statement
+                if place + i > len(currentchoice) - 1:
+                    print(" ", end="")
+                    break
+                print(currentchoice[place + i], end="")
+            if isnew == False:
+                place += int(0.8 * width)
+                for i in range(choiceindent - 1):
+                    print(" ", end="")
+                print(" ")
+            isnew = False
+            for i in range(choiceindent - 1):
+                print(" ", end="")
+            print(" ")
+            if k >= height - 2:
+                print("")
+                for i in range(contindent):
+                    print(" ", end="")
+                input("Press a key") #Also has a newline
+            k += 1
+        print("")
     
 
     
